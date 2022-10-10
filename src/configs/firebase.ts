7b9c235ceee3a3108cadc94c8d0b19d3801b257c
Anuj <anupta45@gmail.com>
2022-10-10 23:12:10 +0530

    chore: auth initialized

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAqw6hyyEtFNQc_fo66DffWQHTDDXK6b_I",
@@ -11,5 +12,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
 
 export default app;
