2722bd8643bdb9fc2ba25023c180d77644720206
Anuj <anupta45@gmail.com>
2022-10-10 22:58:52 +0530

    chore: initial commit

---
+++ src/configs/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAqw6hyyEtFNQc_fo66DffWQHTDDXK6b_I",
+  authDomain: "cycle-project-8e606.firebaseapp.com",
+  projectId: "cycle-project-8e606",
+  storageBucket: "cycle-project-8e606.appspot.com",
+  messagingSenderId: "223601535964",
+  appId: "1:223601535964:web:abcb6b6962767febf1fd19",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
