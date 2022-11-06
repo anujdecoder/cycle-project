d751cf19f6b93e627a62c0707e08aa5beac0abab
Anuj <anupta45@gmail.com>
2022-11-06 23:09:30 +0530

    feat: user mutation added

--- src/containers/users/components/UserForm.tsx
+++ src/containers/users/components/UserForm.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { User } from '../../../types/users'
 import FormDialog from '../../../components/FormDialog'
 import { useForm } from 'react-hook-form'
-import { SignupInput } from '../../../types/auth'
 import { yupResolver } from '@hookform/resolvers/yup/dist/yup'
 import * as yup from 'yup'
-import { useNavigate } from 'react-router-dom'
-import { useMutation } from '@tanstack/react-query'
-import AuthService from '../../../services/AuthService'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { Grid } from '@mui/material'
 import { CheckboxElement, FormContainer, PasswordElement, TextFieldElement } from 'react-hook-form-mui'
+import FirestoreService from '../../../services/FirestoreService'
+import { UsersCollection } from '../../../configs/firestore'
+import { omit, random } from 'lodash-es'
 
 interface Props {
   open: boolean
@@ -20,7 +20,7 @@ interface Props {
 const formId = 'user-form'
 
 const UserForm: React.FC<Props> = ({ open, onClose, user }) => {
-  const formContext = useForm<SignupInput>({
+  const formContext = useForm<User>({
     defaultValues: {
       firstName: '',
       lastName: '',
@@ -45,13 +45,24 @@ const UserForm: React.FC<Props> = ({ open, onClose, user }) => {
       })
     )
   })
-  const navigate = useNavigate()
-  const { mutateAsync, isLoading } = useMutation<any, any, SignupInput, any>(AuthService.register, {
-    mutationKey: ['registerUser'],
-    onSuccess: () => navigate('../')
-  })
 
-  const handleSubmit = async (input: SignupInput) => {
+  const queryClient = useQueryClient()
+  const { mutateAsync, isLoading } = useMutation<any, any, User, any>(
+    (input) =>
+      FirestoreService.createDocument({
+        collection: UsersCollection,
+        document: { ...omit(input, 'password'), id: random().toString() }
+      }),
+    {
+      mutationKey: ['inviteUser'],
+      onSuccess: async () => {
+        await queryClient.invalidateQueries(['listUsers'])
+        onClose()
+      }
+    }
+  )
+
+  const handleSubmit = async (input: User) => {
     await mutateAsync(input)
   }
 
@@ -64,7 +75,7 @@ const UserForm: React.FC<Props> = ({ open, onClose, user }) => {
       loading={isLoading}
       submitLabel={'Invite'}
     >
-      <FormContainer formContext={formContext} onSuccess={handleSubmit}>
+      <FormContainer FormProps={{ id: formId }} formContext={formContext} onSuccess={handleSubmit}>
         <Grid container spacing={2} pt={1}>
           <Grid item xs={12}>
             <TextFieldElement fullWidth name={'firstName'} label={'First Name'} />
